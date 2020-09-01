6d7f6a817c558aac1a417325f09c4962b7f99041
lichangg <479156033@qq.com>
2020-09-01 22:39:57 +0800

    581

--- titles/581. 最短无序连续子数组.py
+++ titles/581. 最短无序连续子数组.py
@@ -2,17 +2,35 @@
 # -*- coding:utf-8 -*-
 from typing import List
 
+# 适配失败，目前过不了[1,2,4,5,3]，放弃适配
+# class Solution:
+#     def findUnsortedSubarray(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         j = 0
+#         max_num = float('-inf')
+#         start = 0
+#         start_flag = False
+#         end = 0
+#         i = 0
+#         while j < n - 1:
+#             max_num = max(max_num, nums[j])
+# 
+#             if nums[j] <= nums[j + 1] and nums[j + 1] >= max_num:
+#                 if nums[j] != nums[j + 1]:
+#                     i = j + 1
+#                 j += 1
+#                 continue
+# 
+#             if not start_flag:
+#                 start = i
+#                 start_flag = True
+#             end = j + 1
+#             j += 1
+#         if not end:
+#             return 0
+#         return end - start + 1
 
-class Solution:
-    def findUnsortedSubarray(self, nums: List[int]) -> int:
-        n = len(nums)
-        temp = []
-        i = 0
-        j=1
-        while j < n:
 
-
-
-b=[2, 6, 4, 8, 10, 9, 15]
-a=Solution().findUnsortedSubarray(b)
+b = [1,2,4,5,3]
+a = Solution().findUnsortedSubarray(b)
 print(a)
