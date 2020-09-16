1ad38abc5422710fc8a7be1b1a5b55fc398e0903
lichenggang <lcg@ichunt.com>
2020-09-16 17:59:18 +0800

    84题

--- titles/79. 单词搜索.py
+++ titles/79. 单词搜索.py
@@ -5,6 +5,9 @@
 # 得到当前字符[A]的坐标集合,从该集合里面的每一个坐标出发,找到其下一个坐标只能出现的上下左右位置,然后和下一个字符B在hashmap里面出现的坐标集合对比看看有没有交集
 # 无交集则该递归停止,有交集则以此递归对比,对比完word所有字符
 # 上述是一刷思路, 可惜超时
+from typing import List
+
+
 class Solution:
 
     def exist(self, board, word: str) -> bool:
@@ -57,7 +60,9 @@ class Solution:
 
         return self.flag
 
-
+# 从每个点出发递归, 代码简洁得多,也不会超时,不过还是慢
+# 执行用时：872 ms, 在所有 Python3 提交中击败了5.01%的用户
+# 内存消耗：18.3 MB, 在所有 Python3 提交中击败了5.05%的用户
 class Solution:
 
     def exist(self, board, word: str) -> bool:
@@ -74,21 +79,51 @@ class Solution:
                             if i[0] == j[0] and i[1] == j[1]:
                                 break
                         else:
-                            path.append((i[0], i[1]))
+                            path.append((x, y))
                             if dfs(n - 1, cur_word_index + 1, i[0], i[1], path[:]):
                                 return True
-                    # dfs(n - 1, cur_word_index + 1, x + 1, y)
-                    # dfs(n - 1, cur_word_index + 1, x - 1, y)
-                    # dfs(n - 1, cur_word_index + 1, x, y + 1)
-                    # dfs(n - 1, cur_word_index + 1, x, y - 1)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(len(word), 0, j, i, []):
+                    return True
 
-        return dfs(len(word), 0, 0, 0, []) or False
+        else:
+            return False
+
+# 别人的递归, 空间上来说貌似上一个算法用了path记录, 而本算法不记录用过的path, 而是用一个used矩阵记录, 对其是否已经用过的状态进行回溯
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m ,n, l = len(board), len(board[0]), len(word)
+        used = [[0]*n for _ in range(m)]
+        bias = [(-1,0), (1,0), (0,-1), (0, 1)]
+        def dfs(c, r, location):
+            nonlocal used
+            flag = False
+            # 判断当前字符是否匹配，若不匹配直接返回，剪枝操作
+            if board[c][r]==word[location]:
+                # 已匹配到最后一个字符且相同，返回True
+                if location==l-1: return True
+                # 当前字符字符匹配成功，后续递归过程中无法再次使用
+                used[c][r] = 1
+                # 对当前字符上下左右四个位置中未匹配的字符进行递归
+                for dx, dy in bias:
+                    x, y = c+dx, r+dy
+                    if 0<=x<m and 0<=y<n and not used[x][y]:
+                        flag = flag or dfs(x, y, location+1)
+                # 回溯状态返回， 此字符可再次被使用
+                used[c][r] = 0
+            return flag
+        # 遍历二维网格中的每一个字符
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0): return True
+        return False
 
 
-board = [["A", "B", "C", "D"],
-         ["S", "F", "C", "S"],
-         ["A", "D", "E", "E"]
-         ]
-
-a = Solution().exist(board, 'SF')
+board = [["A","B","C","E"],
+         ["S","F","C","S"],
+         ["A","D","E","E"]]
+board=[["a","a"]]
+a = Solution().exist(board, 'aaa')
 print(a)
--- titles/84. 柱状图中最大的矩形.py
+++ titles/84. 柱状图中最大的矩形.py
@@ -22,6 +22,10 @@
 # 学到了
 # 思路找到每一个索引向两侧扩散能得到的最大面积, 再在这些最大面积中取最大的
 # 具体操作是以当前i为中心向左右分别找到第一个比heights[i]小的柱子索引,这样就保证了左右两极限索引中间的最大柱子始终为(right_i - left_i - 1) * heights[i]
+# 搞了半天这个方法是超时的,学到个屁
+from typing import List
+
+
 class Solution:
     def largestRectangleArea(self, heights) -> int:
         res = 0
@@ -36,7 +40,8 @@ class Solution:
             res = max(res, (right_i - left_i - 1) * heights[i])
         return res
 
-#单调堆栈,有空看看
+
+# 单调堆栈,有空看看
 # class Solution:
 #     def largestRectangleArea(self, heights: List[int]) -> int:
 #         stack = [-1]
@@ -49,5 +54,84 @@ class Solution:
 #             max_res = max(max_res, heights[stack.pop()]*(len(heights)-1-stack[-1]))
 #         return max_res
 
-a=Solution().largestRectangleArea([2,1,5,6,2,3])
+# 二刷,暴力法, 会超出时间限制
+class Solution:
+    def largestRectangleArea(self, heights) -> int:
+        max_area = 0
+        if not heights: return 0
+
+        def get_area(start, end):
+            return min(heights[start:end + 1]) * (end - start + 1)
+
+        for index, i in enumerate(heights):
+            for j in range(index, len(heights)):
+                max_area = max(max_area, get_area(index, j))
+        return max_area
+
+# 二刷,从每个柱子向两边扩散找到以该柱子为高的最大矩形, 夫再度O(N**2),还是超时
+class Solution:
+    def largestRectangleArea(self, heights) -> int:
+        max_area = 0
+        for index, i in enumerate(heights):
+            l = r = index
+            while l -1>= 0 and heights[l-1] >= i:
+                l -= 1
+            while r+1 < len(heights) and heights[r+1] >= i:
+                r += 1
+            max_area = max(max_area, i * (r - l + 1))
+        # a=Solution().largestRectangleArea([2,1,5,6,2,3])
+        return max_area
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        if not heights:
+            return 0
+        n = len(heights)
+        left_i = [0] * n
+        right_i = [0] * n
+        left_i[0] = -1
+        right_i[-1] = n
+        for i in range(1, n):
+            tmp = i - 1
+            while tmp >= 0 and heights[tmp] >= heights[i]:
+                tmp = left_i[tmp]
+            left_i[i] = tmp
+        for i in range(n - 2, -1, -1):
+            tmp = i + 1
+            while tmp < n and heights[tmp] >= heights[i]:
+                tmp = right_i[tmp]
+            right_i[i] = tmp
+        # print(left_i)
+        # print(right_i)
+        res = 0
+        for i in range(n):
+            res = max(res, (right_i[i] - left_i[i] - 1) * heights[i])
+        return res
+
+
+# 虽然代码不是这个题解的,但是思路是, 写的很好
+# https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/bao-li-jie-fa-zhan-by-liweiwei1419/
+# 主题思路仍然是遍历数组找到[以该值为高度的最大矩形面积], 之前的方式是左右指针向两边扩散, 相当于又遍历一遍,所以复杂度O(N**2)
+# 既然目的是找到[以该值为高度的最大矩形面积], 以[2,1,5,6,2,3], 遍历到2时,尚且不知道以2为高度的最大矩形面积,但是遍历到1时就知道了(但是以1为高度的最大面积还是不知道的)
+# 然后遍历到5(此时1,5都不知道), 再到6(1,5,6都不知道), 但是到2的时候(6可以弹出来, 因为它的max_area知道了, 5也可以弹出来, 但是1仍然是不知道的)
+# 此种方式完美符合栈的结构, 所以可以结合单调栈优化(保持栈内最后一个元素始终是最大)
+# 不过这里还存在栈内存索引还是存数据的问题, 存索引就好了
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        # 此处首尾添加0非常精妙
+        heights = [0] + heights + [0]
+        res = 0
+        for i in range(len(heights)):
+            #print(stack)
+            # 由于单调栈的特性, 处于stack最后一个位置的元素肯定是比其前面的元素都要大的, 在往后遍历的过程中,
+            # 当找到了比该元素小的值时则可以说找到了以该元素的值为高度的最大矩形面积
+            while stack and heights[stack[-1]] > heights[i]:
+                tmp = stack.pop()
+                res = max(res, (i - stack[-1] - 1) * heights[tmp])
+            stack.append(i)
+        return res
+
+a = Solution().largestRectangleArea([2,1,5,6,2,3])
 print(a)
