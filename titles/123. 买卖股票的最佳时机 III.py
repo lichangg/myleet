c4e30ea113b5183f7d27c71736a13dd4f971ab8e
lichang <479156033@qq.com>
2021-01-28 09:24:35 +0800

    dp

--- titles/123. 买卖股票的最佳时机 III.py
+++ titles/123. 买卖股票的最佳时机 III.py
@@ -6,21 +6,35 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l = len(prices)
-        dp = [[0]*4 for _ in range(l)]
-        dp[0][0], dp[0][1], dp[0][2], dp[0][3] = -prices[0], -prices[0], 0, 0
+        dp = [[0]*5 for _ in range(l)]
+        # 状态0表示第一次交易中有了股票, 1表示第一次交易已经完成, 2表示第二次交易中有了股票, 3表示第二次交易已经完成, 4表示不做任何操作(该状态可以去掉)
+        dp[0][0], dp[0][1], dp[0][2], dp[0][3], dp[0][4] = -prices[0], 0, -prices[0], 0, 0
         i = 1
         max_profit = 0
         while i<l:
-            dp[i][0] = max(dp[i-1][2], dp[i-1][3]) - prices[i]
-            dp[i][1] = max(dp[i-1][0], dp[i-1][1])
-            dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + prices[i]
-            dp[i][3] = max(dp[i-1][2], dp[i-1][3])
-            max_profit = max(dp[i][2], dp[i][3], max_profit)
+            dp[i][0] = max(- prices[i], dp[i-1][0])
+            dp[i][1] = max(dp[i-1][0] + prices[i], dp[i-1][1])
+            dp[i][2] = max(dp[i-1][1] - prices[i], dp[i-1][2])
+            dp[i][3] = max(dp[i-1][2] + prices[i], dp[i-1][3], dp[i-1][4])
+            max_profit = max(max_profit, dp[i][3])
+            dp[i][4] = max(dp[i-1][4], dp[i-1][1])
             i+=1
-        while i<l:
+
+        return max_profit
+
+# 压缩了空间
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
 
 
-
-
-a=Solution().maxProfit([3,3,5,0,0,3,1,4])
+a=Solution().maxProfit(prices = [1,2,3,4,5])
 print(a)
--- titles/188. 买卖股票的最佳时机 IV.py
+++ titles/188. 买卖股票的最佳时机 IV.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#todo
+# todo
 from typing import List
 
 
+# 设置sell[i][j]为对于数组前i个价格, 交易j次, 且手上没有股票的状态
+# 设置buy[i][j]为对于数组前i个价格, 交易j次, 且手上有股票的状态
+# 状态转移buy[i][j] = max(sell[i-1][j], buy[i-1][j] - prices[i])
+# 状态转移sell[i][j] = max(sell[i-1][j], buy[i-1][j] - prices[i])
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        l = len(prices)
+        sell = [[0] * k for _ in range(l)]
+        buy = [[0] * k for _ in range(l)]
+        sell[0][0] = 0
+        buy[0][0] = -prices[0]
+        i = 1
+        while i < l:
+            for j in range(k):
+                buy[i][j] = max(sell[i - 1][j] - prices[i], buy[i - 1][j])
+                sell[i][j] = max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i])
+            i += 1
+        return max(sell[-1])
 
+
+a = Solution().maxProfit(k=2, prices=[3, 2, 6, 5, 0, 3])
+print(a)
